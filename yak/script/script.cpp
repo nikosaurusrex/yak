7dc9c0a598bf4313c5aa76ccf2fcfb5af356dc3f
cactulus <Nikolas@Kaipel.net>
2023-09-23 16:48:24 +0200

    glad

--- yak/script/script.cpp
+++ yak/script/script.cpp
@@ -1,12 +1,15 @@
 #include "script.h"
 
+#ifndef _WIN64
 #include <dlfcn.h>
+#endif
 
 Script::Script(string path)
 	: path(path) {
 }
 
 void Script::reload() {
+#ifndef _WIN64
 	void *script_so = dlopen(path.c_str(), RTLD_NOW);
 
 	ScriptableEntity entity;
@@ -15,4 +18,5 @@ void Script::reload() {
 	entity.update_function = (update_function_type) dlsym(script_so, "update");
 
 	dlclose(script_so);
+#endif
 }
